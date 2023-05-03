df2f6dd828bf4cef6a178dd645c84d8bbc4730b7
Arianna Ramon <bananafish@outlook.es>
2023-05-03 14:58:38 -0500

    Creacion de las cards de recursos

---
+++ src/app/components/card-resources/card-resources.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-resources',
+  templateUrl: './card-resources.component.html',
+  styleUrls: ['./card-resources.component.css']
+})
+export class CardResourcesComponent {
+
+}
