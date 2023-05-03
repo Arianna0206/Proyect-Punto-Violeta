524be6f2e38fd8b96bcb0a1405a4786f03cbadf9
Arianna Ramon <bananafish@outlook.es>
2023-05-03 13:07:10 -0500

    seccion de quienes somos terminada

--- src/app/components/content-card/content-card.component.ts
+++ src/app/components/content-card/content-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.css']
 })
-export class ContentCardComponent {
+export class ContentCardComponent{
+  cardVacia:boolean = false;
+
+vaciarCardPrincipal() {
+  this.cardVacia = true;
+}
+
+volverCardPrincipal() {
+  this.cardVacia = false;
+}
 
 }
+
